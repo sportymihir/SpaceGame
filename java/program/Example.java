bdf51d196856ca2e64aa8894e9fc453f57fe5d17
Mihir Mahale <mahalems1@s.dcsdk12.org>
2018-01-11 12:02:09 -0700

    You're The Best

---
+++ java/program/Example.java
@@ -0,0 +1,23 @@
+
+class Example {
+
+	public static void main(String[] args) {
+		for (String str : args) {
+		System.out.println(args);
+
+		}
+
+		int n = Integer.parseInt(args[0]);
+		double a = Double.parseDouble(args[0]);
+		
+		
+		for (int i=0; i<5; i++) {
+		System.out.println("YOU'RE THE BEST");
+	}
+
+
+		
+		
+
+	}
+}
