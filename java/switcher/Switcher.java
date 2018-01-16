3bd4fd70a95564ce66d90a19c05ef3ccc1556b13
Mihir Mahale <mahalems1@s.dcsdk12.org>
2018-01-16 11:15:45 -0700

    Work In Progress for Switcher

---
+++ java/switcher/Switcher.java
@@ -0,0 +1,15 @@
+class Switcher {
+	public static void main(String[] args) {
+
+
+	}
+
+	public static String switcher(String initial, String swapA, String swapB) {
+		String str = "fdgisufhd"
+		str.substring(i, i+swapA.length));
+
+		str.eql("stdyu");
+
+		if ($$$$$$$$ = 800 dollars, how much does $$$$$$$$$$$$$$ equal?)
+}
+
