88e9161e02df83f2c04a3547ae317315f167b07e
Mihir Mahale <mahalems1@s.dcsdk12.org>
2018-02-05 11:32:00 -0700

    Exercise 2

---
+++ java_practice/Exercise2.java
@@ -0,0 +1,26 @@
+class Exercise2
+{
+  public static void main ( String[] args )
+  {
+    int[] val = {13, -4, 82, 17}; 
+    int[] twice = new int[4];
+    
+    System.out.println( "Original Array: " 
+        + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+ 
+    // Construct an array object for twice.
+
+    for (int i=0; i<4; i++) {
+      twice[i] += (val[i] * 2);
+    }
+
+    
+    // Put values in twice that are twice the
+    // corresponding values in val.
+
+
+ 
+    System.out.println( "New Array: " 
+        + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+   }
+}
