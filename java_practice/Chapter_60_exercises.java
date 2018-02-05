6a01705abd2d6eb80bbcd4ed9c0c7338880d5939
Mihir Mahale <mahalems1@s.dcsdk12.org>
2018-02-05 11:11:58 -0700

    Chapter 60 exercise 1

---
+++ java_practice/Chapter_60_exercises.java
@@ -0,0 +1,12 @@
+class Chapter_60_exercises
+{
+  public static void main ( String[] args )
+  {
+    int[] val = {0, 1, 2, 3}; 
+
+    sum = val[0] + val[1] + val[2] + [3];
+ 
+    System.out.println( "Sum of all numbers = " + sum );
+ 
+   }
+}
